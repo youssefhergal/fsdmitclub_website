b010ec3457691400dc34a406f817bba4d3878d00
Youssef Hergal <110983974+youssefhergal@users.noreply.github.com>
2024-10-07 20:45:13 +0100

    Update HeroStyle.js

--- src/components/HeroSection/HeroStyle.js
+++ src/components/HeroSection/HeroStyle.js
@@ -6,7 +6,7 @@ export const HeroContainer = styled.div`
     background-position: center; /* Centers the image */
     background-repeat: no-repeat; /* Prevents the image from repeating */
     background: rgb(255, 255, 255);
-    background: linear-gradient(90deg, rgba(255,255,255,1) 24%, rgba(47,78,161,0.6134103299522935) 60%), url("/src/assets/back.jpeg");
+    background: linear-gradient(90deg, rgba(255,255,255,1) 24%, rgba(47,78,161,0.6134103299522935) 60%), url("./assets/back.jpeg");
 
 
     background-size: cover; /* Ensures the image covers the entire container */
