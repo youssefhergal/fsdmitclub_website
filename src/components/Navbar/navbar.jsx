3472cd77514a2dcdfaa021ddb6683e1356a220a3
Dabaghi-Anass <anass.debbaghi123@gmail.com>
2024-11-17 15:01:44 +0100

    changed variable name

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,60 +1,107 @@
-import React from 'react'
-import { Nav, NavLink, NavbarContainer, NavLogo, NavItems, OpenPositionBtn, ButtonContainer, MobileIcon, MobileMenu, MobileLink } from './NavbarStyledComponent'
-import { FaBars } from 'react-icons/fa';
-import usmbaLogo from '../../assets/images/Fsdm_it_club_logo.png';
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+	ButtonContainer,
+	MobileIcon,
+	MobileLink,
+	MobileMenu,
+	Nav,
+	NavbarContainer,
+	NavItems,
+	NavLink,
+	NavLogo,
+	OpenPositionBtn,
+} from "./NavbarStyledComponent";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = React.useState(false);
-  return (
-      <Nav>
-        <NavbarContainer>
-          <NavLogo to='/'>
-            <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20px', cursor: 'pointer' }}>
-              <img src={usmbaLogo} alt="Logo" style={{ marginTop : '10px' , width: '90px', height: 'auto', backgroundColor: 'transparent' }} />
-            </a>
-          </NavLogo>
+	const [isOpen, setIsOpen] = React.useState(false);
+	return (
+		<Nav>
+			<NavbarContainer>
+				<NavLogo to='/'>
+					<a
+						style={{
+							display: "flex",
+							alignItems: "center",
+							color: "white",
+							marginBottom: "20px",
+							cursor: "pointer",
+						}}>
+						{/* <img src={usmbaLogo} alt="Logo" style={{ marginTop : '10px' , width: '90px', height: 'auto', backgroundColor: 'transparent' }} /> */}
+					</a>
+				</NavLogo>
 
-          <MobileIcon>
-            <FaBars onClick={() => {
-              setIsOpen(!isOpen)
-            }} />
-          </MobileIcon>
-          <NavItems>
-            <NavLink href="#">Home</NavLink>
-            <NavLink href='#about'>About</NavLink>
-            <NavLink href='#team'>Team</NavLink>
-            <NavLink href='#events'>Events</NavLink>
-            <NavLink href='#program'>Program</NavLink>
-          </NavItems>
-          <ButtonContainer>
-            <OpenPositionBtn href={"#"} target="_blank">Open Position</OpenPositionBtn>
-          </ButtonContainer>
-          {
-              isOpen &&
-              <MobileMenu isOpen={isOpen}>
-                <MobileLink href="#" onClick={() => {
-                  setIsOpen(!isOpen)
-                }}>Home</MobileLink>
-                <MobileLink href='#about' onClick={() => {
-                  setIsOpen(!isOpen)
-                }}>About</MobileLink>
-                <MobileLink href='#team' onClick={() => {
-                  setIsOpen(!isOpen)
-                }}>Team</MobileLink>
-                <MobileLink href='#events' onClick={() => {
-                  setIsOpen(!isOpen)
-                }}>Events</MobileLink>
-                <MobileLink href='#program' onClick={() => {
-                  setIsOpen(!isOpen)
-                }}>Program</MobileLink>
-                <OpenPositionBtn style={{padding: '10px 16px', color: 'white',width: 'max-content'}} href={"#"} target="_blank">Open Position</OpenPositionBtn>
-              </MobileMenu>
-          }
-        </NavbarContainer>
-      </Nav>
+				<MobileIcon>
+					<FaBars
+						onClick={() => {
+							setIsOpen(!isOpen);
+						}}
+					/>
+				</MobileIcon>
+				<NavItems>
+					<NavLink href='#'>Home</NavLink>
+					<NavLink href='#about'>About</NavLink>
+					<NavLink href='#team'>Team</NavLink>
+					<NavLink href='#events'>Events</NavLink>
+					<NavLink href='#program'>Program</NavLink>
+				</NavItems>
+				<ButtonContainer>
+					<OpenPositionBtn href={"#"} target='_blank'>
+						Open Position
+					</OpenPositionBtn>
+				</ButtonContainer>
+				{isOpen && (
+					<MobileMenu isOpen={isOpen}>
+						<MobileLink
+							href='#'
+							onClick={() => {
+								setIsOpen(!isOpen);
+							}}>
+							Home
+						</MobileLink>
+						<MobileLink
+							href='#about'
+							onClick={() => {
+								setIsOpen(!isOpen);
+							}}>
+							About
+						</MobileLink>
+						<MobileLink
+							href='#team'
+							onClick={() => {
+								setIsOpen(!isOpen);
+							}}>
+							Team
+						</MobileLink>
+						<MobileLink
+							href='#events'
+							onClick={() => {
+								setIsOpen(!isOpen);
+							}}>
+							Events
+						</MobileLink>
+						<MobileLink
+							href='#program'
+							onClick={() => {
+								setIsOpen(!isOpen);
+							}}>
+							Program
+						</MobileLink>
+						<OpenPositionBtn
+							style={{
+								padding: "10px 16px",
+								color: "white",
+								width: "max-content",
+							}}
+							href={"#"}
+							target='_blank'>
+							Open Position
+						</OpenPositionBtn>
+					</MobileMenu>
+				)}
+			</NavbarContainer>
+		</Nav>
+	);
+};
 
-
-  )
-}
-
-export default Navbar
+export default Navbar;
